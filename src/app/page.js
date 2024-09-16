2d1febace184448f84c95c8263556d721bc070c1
daniel <danielpizarrodev@gmail.com>
2024-09-16 20:11:59 -0300

    :recycle: Add better implementation

--- src/app/page.js
+++ src/app/page.js
@@ -34,27 +34,25 @@ function Board({ xIsNext, squares, onPlay }) {
     status = "Next player →";
   }
 
-  const aux = status == "Winner: "
-    ? <> {status} <code>{winner}</code> </>
-    : <> {status} <code>{(xIsNext ? "X" : "O")}</code> </>
-
   return (
     <>
-      <div className={styles.status}>{aux}</div>
-      <div className={styles.row}>
-        <Square value={squares[0]} onSquareClick={() =>handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() =>handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() =>handleClick(2)} />
+      <div className={styles.status}>
+        {status} <code>{winner ? winner : xIsNext ? "X" : "O"}</code>
       </div>
       <div className={styles.row}>
-        <Square value={squares[3]} onSquareClick={() =>handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() =>handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() =>handleClick(5)} />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className={styles.row}>
-        <Square value={squares[6]} onSquareClick={() =>handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() =>handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() =>handleClick(8)} />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className={styles.row}>
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
